fe8c4a808e8dcb4dc8a2d6d3e8b664aad0d4828f
farisgp <farisghina51@gmail.com>
2025-01-18 21:03:54 +0700

    updated

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use App\Http\Controllers\PDFController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::redirect('/', '/admin/login');
 Route::get('PDFRekapDropping', [PDFController::class, 'generatePDFRekapDropping'])->name('PDFRekapDropping');
 Route::get('PDFRekapLapak', [PDFController::class, 'generatePDFRekapLapak'])->name('PDFRekapLapak');
